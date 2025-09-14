fe1fad1b6434cba4e0a568148af3fb2b33dda675
nealoverton <nealjoverton@gmail.com>
2025-09-14 19:00:12 +0100

    Add Road Vehicles enum

---
+++ src/main/java/Journey/RoadVehicles.java
@@ -0,0 +1,34 @@
+package Journey;
+
+public enum RoadVehicles {
+    CAR("Car", 4, 0.2, 3),
+    TAXI("Taxi", 4, 0.4, 0);
+
+    private final String displayName;
+    private final int maxPassengers;
+    private final double costPerMileInPounds;
+    private final int parkingFee;
+
+    RoadVehicles(String displayName, int maxPassengers, double costPerMileInPounds, int parkingFee) {
+        this.displayName = displayName;
+        this.maxPassengers = maxPassengers;
+        this.costPerMileInPounds = costPerMileInPounds;
+        this.parkingFee = parkingFee;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    public double getCostPerMileInPounds() {
+        return costPerMileInPounds;
+    }
+
+    public int getParkingFee() {
+        return parkingFee;
+    }
+}
