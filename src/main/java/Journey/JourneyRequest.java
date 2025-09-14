94e4cf3b34c7119f9f744fa9d0a4fb75167dd23a
nealoverton <nealjoverton@gmail.com>
2025-09-14 21:55:43 +0100

    handle invalid inputs

--- src/main/java/Journey/JourneyRequest.java
+++ src/main/java/Journey/JourneyRequest.java
@@ -1,29 +1,39 @@
 package Journey;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JourneyRequest {
-    private int passengers;
-    private int homeAirportDistanceMiles;
+    private Integer passengers;
+    private Integer homeAirportDistanceMiles;
     private String homeAirport;
     private String destinationAirport;
 
-    private JourneyRequest(int passengers, int homeAirportDistanceMiles, String homeAirport, String destinationAirport) {
+    private JourneyRequest(Integer passengers, Integer homeAirportDistanceMiles, String homeAirport, String destinationAirport) {
         this.passengers = passengers;
         this.homeAirportDistanceMiles = homeAirportDistanceMiles;
         this.homeAirport = homeAirport;
         this.destinationAirport = destinationAirport;
     }
 
-    public static Optional<JourneyRequest> fromInput(int passengers, String homeAirportJourney, String destinationAirport) {
+    public static Optional<JourneyRequest> fromInput(Integer passengers, String homeAirportJourney, String destinationAirport) {
+        if (passengers == null
+                || passengers <= 0
+                || StringUtils.isBlank(homeAirportJourney)
+                || StringUtils.isBlank(destinationAirport)
+        ) {
+            return Optional.empty();
+        }
+
         Pattern homeAirportJourneyPattern = Pattern.compile("^([A-Z]+)(\\d+)$");
         Matcher homeAirportJourneyMatcher = homeAirportJourneyPattern.matcher(homeAirportJourney);
         if (homeAirportJourneyMatcher.matches()) {
             try {
                 String homeAirport = homeAirportJourneyMatcher.group(1);
-                int homeAirportDistanceMiles = Integer.parseInt(homeAirportJourneyMatcher.group(2));
+                Integer homeAirportDistanceMiles = Integer.parseInt(homeAirportJourneyMatcher.group(2));
                 return Optional.of(new JourneyRequest(passengers, homeAirportDistanceMiles, homeAirport, destinationAirport));
             } catch (NumberFormatException exception) {
                 return Optional.empty();
@@ -32,35 +42,19 @@ public class JourneyRequest {
         return Optional.empty();
     }
 
-    public int getPassengers() {
+    public Integer getPassengers() {
         return passengers;
     }
 
-    public void setPassengers(int passengers) {
-        this.passengers = passengers;
-    }
-
-    public int getHomeAirportDistanceMiles() {
+    public Integer getHomeAirportDistanceMiles() {
         return homeAirportDistanceMiles;
-    }
-
-    public void setHomeAirportDistanceMiles(int homeAirportDistanceMiles) {
-        this.homeAirportDistanceMiles = homeAirportDistanceMiles;
     }
 
     public String getHomeAirport() {
         return homeAirport;
     }
 
-    public void setHomeAirport(String homeAirport) {
-        this.homeAirport = homeAirport;
-    }
-
     public String getDestinationAirport() {
         return destinationAirport;
-    }
-
-    public void setDestinationAirport(String destinationAirport) {
-        this.destinationAirport = destinationAirport;
     }
 }
--- src/test/java/JourneyRequestTest.java
+++ src/test/java/JourneyRequestTest.java
@@ -1,8 +1,14 @@
+import Journey.JourneyBuilder;
 import Journey.JourneyRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,10 +25,42 @@ public class JourneyRequestTest {
         assertEquals(20, journeyRequest.getHomeAirportDistanceMiles());
     }
 
-    @Test
     @DisplayName("Fails to instantiate with invalid homeAirportJourney input")
-    public void testUnsuccessfulInstantiation() {
-        Optional<JourneyRequest> requestJourneyOptional = JourneyRequest.fromInput(2, "B20dfgds", "D");
+    @ParameterizedTest(name = "Test invalid homeAirportJourney input: {0}")
+    @ValueSource(strings = {"B20dfgds", "9B20", "a10", "100", "aaa", ""})
+    public void testUnsuccessfulInstantiationWithInvalidHomeAirportJourney(String invalidInput) {
+        Optional<JourneyRequest> requestJourneyOptional = JourneyRequest.fromInput(2, invalidInput, "D");
         assertTrue(requestJourneyOptional.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Fails to instantiate with empty destinationAirport input")
+    public void testUnsuccessfulInstantiationWithEmptyDestinationAirport() {
+        Optional<JourneyRequest> requestJourneyOptional = JourneyRequest.fromInput(2, "B20", "");
+        assertTrue(requestJourneyOptional.isEmpty());
+    }
+
+    @DisplayName("Fails to instantiate with non-positive passengers input")
+    @ParameterizedTest(name = "Test invalid passengers input: {0}")
+    @ValueSource(ints = {0, -1})
+    public void testUnsuccessfulInstantiationWithInvalidPassengers(Integer invalidInput) {
+        Optional<JourneyRequest> requestJourneyOptional = JourneyRequest.fromInput(invalidInput, "B20", "D");
+        assertTrue(requestJourneyOptional.isEmpty());
+    }
+
+    @DisplayName("Fails to instantiate with null input")
+    @ParameterizedTest(name = "Test null arguments: passengers={0}, homeAirportJourney={1}, destinationAirport={2}")
+    @MethodSource("nullArgumentsProvider")
+    public void testUnsuccessfulInstantiationWithNullValues(Integer passengers, String homeAirportJourney, String destinationAirport) {
+        Optional<JourneyRequest> requestJourneyOptional = JourneyRequest.fromInput(passengers, homeAirportJourney, destinationAirport);
+        assertTrue(requestJourneyOptional.isEmpty());
+    }
+
+    private static Stream<Arguments> nullArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(null, "B20", "D"),
+                Arguments.of(2, null, "D"),
+                Arguments.of(2, "B20", null)
+        );
     }
 }
